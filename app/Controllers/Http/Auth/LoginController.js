fc1624d2a360c28b47246a155b5282049fe944bf
artemOktisyuk <artem@wetelo.com>
2019-03-07 16:10:46 +0200

    Create connect user [frontend]

--- app/Controllers/Http/Auth/LoginController.js
+++ app/Controllers/Http/Auth/LoginController.js
@@ -50,10 +50,52 @@ class LoginController {
     //
     // Display error message
     //
-    return response.status(400).send({
+    return response.status(500).send({
       type: 'danger',
       message: `We couldn't verify user credentials. Make sure you've confirmed your email address`
     })
+  }
+
+  async getUserData ({
+    request,
+    auth,
+    response
+  }) {
+    //
+    // Get user token
+    //
+    const token = request.input('token')
+    if (!token) {
+      return response.status(500).send({
+        type: 'danger',
+        message: 'No user token'
+      })
+    }
+    
+    //
+    // Get user data by token
+    //
+    const user = await User.query()
+      .where('register_user_token', token)
+      .where('is_active', true)
+      .first()
+    const allUsers = await User.all()
+    if (user) {
+      return response.status(200).send({
+        type: 'success',
+        user: {
+          id: user.id,
+          name: user.username,
+          created: user.created_at,
+          allUsers: allUsers
+        }
+      })
+    } else {
+      return response.status(500).send({
+        type: 'danger',
+        message: 'No find user'
+      })
+    }
   }
 }
 
